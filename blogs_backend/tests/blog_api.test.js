13c07422dde7b9c419f0bf366b6fb2c6647d4c60
edwinalbanez <edwinalbanez26@gmail.com>
2025-09-21 00:12:04 -0600

    4.8: Blog List Tests, step 1

---
+++ blogs_backend/tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const { test, beforeEach, after } = require('node:test');
+const mongoose = require('mongoose');
+const assert = require('node:assert');
+const app = require('../app');
+const supertest = require('supertest');
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+beforeEach( async () => {
+  await Blog.deleteMany({});
+
+  const blogs = helper.initialBlogs.map(blog => new Blog(blog));
+  const promiseArray = blogs.map(blog => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test('Blogs are returned as JSON', async () => {
+  const { body: blogs } = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const expectedQuantity = helper.initialBlogs.length;
+  assert.strictEqual(blogs.length, expectedQuantity);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+})
---
+++ blogs_backend/tests/test_helper.js
@@ -0,0 +1,53 @@
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+  {
+    title: "Introduction to React Hooks",
+    author: "María González",
+    url: "https://blog.ejemplo.com/react-hooks-intro",
+    likes: 245
+  },
+  {
+    title: "Node.js: Building REST APIs",
+    author: "Diego López",
+    url: "https://backend.ejemplo.com/nodejs-apis-rest",
+    likes: 156
+  },
+  {
+    title: "MongoDB: NoSQL Data Modeling",
+    author: "Roberto Silva",
+    url: "https://database.ejemplo.com/mongodb-modelado",
+    likes: 134
+  },
+  {
+    title: "JWT Authentication in Web Applications",
+    author: "Patricia Morales",
+    url: "https://seguridad.ejemplo.com/autenticacion-jwt",
+    likes: 201
+  },
+  {
+    title: "Docker for JavaScript Developers",
+    author: "Miguel Torres",
+    url: "https://devops.ejemplo.com/docker-javascript",
+    likes: 167
+  },
+];
+
+const fakeId = async () => {
+  const blog = new Blog({ title: 'delete me' });
+  await blog.save();
+  await blog.deleteOne();
+
+  return blog._id.toString();
+}
+
+const blogsInDB = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+}
+
+module.exports = {
+  initialBlogs,
+  fakeId,
+  blogsInDB
+}
